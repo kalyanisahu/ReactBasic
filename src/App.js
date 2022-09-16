0e05363f57041716323cb379890cb1068a5f6fda
kalyani <kalyanisahu03@gmail.com>
2022-09-17 00:42:24 +0530

    Uncommendetd counter functions

--- src/App.js
+++ src/App.js
@@ -4,37 +4,35 @@ import "./App.css";
 import Counter from "./Counter/Counter";
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     name: "nitesh",
-  //     counter: -1,
-  //   };
-  // }
-  componentDidMount() {
-    console.log("componentdimount");
+  constructor() {
+    super();
+    this.state = {
+      name: "nitesh",
+      counter: -1,
+    };
   }
-  // handleShowAlert = () => {
-  //   alert("Hello Everyone!");
-  // };
-  // handleAddition = () => {
-  //   this.setState({ counter: this.state.counter + 1 });
-  // };
-  // handleSubtract = () => {
-  //   this.setState({ counter: this.state.counter - 1 });
-  // };
+
+  handleShowAlert = () => {
+    alert("Hello Everyone!");
+  };
+  handleAddition = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  handleSubtract = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
   render() {
     console.log("props", this.props);
     return (
       <div className="App" key="1">
-        {/* <button onClick={this.handleShowAlert}>Show Alert</button>
+        <button onClick={this.handleShowAlert}>Show Alert</button>
         <div>
           <button onClick={this.handleAddition}>ADD</button>
           {/* <div>{this.state.counter}</div> */}
-        {/* <Counter counterValue={this.state.counter} key="1" /> */}
-        {/* <button onClick={this.handleSubtract}>Sub</button>
-        </div> */}
+          <Counter counterValue={this.state.counter} key="1" />
+          <button onClick={this.handleSubtract}>Sub</button>
+        </div>
       </div>
     );
   }
